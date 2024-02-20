2a8366c78475c8895b45300c58db0fe649494627
Md Tanjim Ahmmed <tanjimahmmed.8@gmail.com>
2024-02-20 19:57:23 +0600

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-import express from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.send("Welcome to Auth service");
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    // if(err instanceof Error){
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
+    // }
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ const startServer = () => {
     try {
         // throw new Error("Something went wrong");
         // logger.debug("debug message", {});
+
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
         if (err instanceof Error) {
