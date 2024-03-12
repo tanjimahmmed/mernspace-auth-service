ba7bf653a08fe229a5573e808be742e31d482ce7
Md Tanjim Ahmmed <tanjimahmmed.8@gmail.com>
2024-03-12 14:37:27 +0600

    add error handling and logging

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,13 +1,39 @@
 import { Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
+import { NextFunction } from "express-serve-static-core";
+import { Logger } from "winston";
 
 export class AuthController {
-    constructor(private userService: UserService) {}
-    async register(req: RegisterUserRequest, res: Response) {
-        const { firstName, lastName, email, password } = req.body;
-        await this.userService.create({ firstName, lastName, email, password });
+    constructor(
+        private userService: UserService,
+        private logger: Logger,
+    ) {}
 
-        res.status(201).json();
+    async register(
+        req: RegisterUserRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
+        const { firstName, lastName, email, password } = req.body;
+        this.logger.debug("New request to register a user", {
+            firstName,
+            lastName,
+            email,
+            password: "********",
+        });
+        try {
+            const user = await this.userService.create({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
+            this.logger.info("User has been registered", { id: user.id });
+            res.status(201).json({ id: user.id });
+        } catch (err) {
+            next(err);
+            return;
+        }
     }
 }
