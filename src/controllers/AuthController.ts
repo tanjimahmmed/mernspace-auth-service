94a0b80d3905aac94cc8bdf6480fa2dff7f36197
Md Tanjim Ahmmed <tanjimahmmed.8@gmail.com>
2024-03-25 14:46:43 +0600

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -6,8 +6,6 @@ import { NextFunction } from "express-serve-static-core";
 import { Logger } from "winston";
 // import createHttpError from "http-errors";
 import { validationResult } from "express-validator";
-import { AppDataSource } from "../config/data-source";
-import { RefreshToken } from "../entity/RefreshToken";
 import { TokenService } from "../services/TokenService";
 
 export class AuthController {
@@ -75,13 +73,16 @@ export class AuthController {
             const accessToken = this.tokenService.generateAccessToken(payload);
 
             // persist the rfresh token
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
+
+            // const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+            // const refreshTokenRepository =
+            //     AppDataSource.getRepository(RefreshToken);
+            // const newRefreshToken = await refreshTokenRepository.save({
+            //     user: user,
+            //     expiresAt: new Date(Date.now() + MS_IN_YEAR),
+            // });
 
             // const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
             //     algorithm: "HS256",
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,11 +7,13 @@ import logger from "../config/logger";
 import { body } from "express-validator";
 import registerValidator from "../validators/register-validator";
 import { TokenService } from "../services/TokenService";
+import { RefreshToken } from "../entity/RefreshToken";
 
 const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const tokenService = new TokenService();
+const refreshTokenRepository = AppDataSource.getRepository(RefreshToken);
+const tokenService = new TokenService(refreshTokenRepository);
 const authController = new AuthController(userService, logger, tokenService);
 
 router.post(
