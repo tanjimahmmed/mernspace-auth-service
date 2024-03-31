5a91e544e0bef634cbf7e31e185f1942986c280e
Md Tanjim Ahmmed <tanjimahmmed.8@gmail.com>
2024-03-31 17:05:22 +0600

    convert pem file to json

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -5,7 +5,7 @@ import rsaPemToJwk from "rsa-pem-to-jwk";
 const privateKey = fs.readFileSync("./certs/private.pem");
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const jwk = rsaPemToJwk(privateKey, {use: "sig"}, "public");
+const jwk = rsaPemToJwk(privateKey, { use: "sig" }, "public");
 
 // eslint-disable-next-line no-undef
 console.log(JSON.stringify(jwk));
