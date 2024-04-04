b7a7c7d1a750a86e8367d69bd1b0ebb1665df45e
Md Tanjim Ahmmed <tanjimahmmed.8@gmail.com>
2024-04-04 21:05:59 +0600

    add access middleware

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -6,6 +6,8 @@ import { Tenant } from "../entity/Tenant";
 import logger from "../config/logger";
 
 import authenticate from "../middlewares/authenticate";
+import { canAccess } from "../middlewares/canAccess";
+import { Roles } from "../constants";
 
 const router = express.Router();
 
@@ -13,7 +15,7 @@ const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
 const tenantController = new TenantController(tenantService, logger);
 
-router.post("/", authenticate, (req, res, next) =>
+router.post("/", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
     tenantController.create(req, res, next),
 );
 
